e5147ed064e73a575d99632359588dda9698ec40
Phillip Sommerfeld <sommerfeldphillip@gmail.com>
2021-11-22 12:31:08 +0000

    pointless first push

---
+++ db/migrate/20211122120826_create_offers.rb
@@ -0,0 +1,14 @@
+class CreateOffers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :offers do |t|
+      t.string :item_name
+      t.integer :price
+      t.string :image_url
+      t.string :description
+      t.integer :quantity
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20211122121250_create_transactions.rb
@@ -0,0 +1,11 @@
+class CreateTransactions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :transactions do |t|
+      t.references :offer, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.integer :item_price
+
+      t.timestamps
+    end
+  end
+end
